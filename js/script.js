73834163e79a9f31011369298aadbcfc2c983432
papschulo <ockyyudah7@gmail.com>
2025-12-22 20:49:44 +0700

    menambahkan hamburger menu dan merevisi sebagian code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 const btn = document.querySelector(".btn");
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".navbar ul");
+
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
 
 btn.classList.toggle("btn--loading");
